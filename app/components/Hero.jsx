4fb895d27a87cfef59db3d98a3e2f50cbd02fad8
ErikRobles <erikjames69@hotmail.com>
2023-06-17 21:13:18 -0600

    fixed missing overlay hero image

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,4 +1,4 @@
-"use client" 
+"use client";
 import Image from "next/image";
 import React from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
@@ -7,30 +7,35 @@ import { useRouter } from "next/navigation";
 const Hero = () => {
   const router = useRouter();
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img text-center">
+    <div className="relative flex flex-col items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img text-center">
       {/* overlay */}
-      <div className="absolute top-0 left-0 right-0 bottom-0 h-full bg-black/70 z-[2]">
-      <div className="p-5 text-white z-[2]">
+      <div className="absolute inset-0 bg-black/70 z-0"></div>
+      <div className="p-5 text-white z-20">
         <Image
           src="/images/logo2.webp"
           width={500}
           height={500}
           alt="Radgnarack"
           priority={true}
+         
         />
-         </div>
-        <div className="p-5 text-white z-[2]">
-        <p className="py-6 text-2xl">Racks that make your life easier through mechanics and metal</p>
-        <p><span className="uppercase font-bold text-2xl">Radgnarack Electric Bike Rack</span></p>
+      </div>
+      <div className="p-5 text-white z-10">
+        <p className="py-6 text-2xl">
+          Racks that make your life easier through mechanics and metal
+        </p>
+        <p>
+          <span className="uppercase font-bold text-2xl">
+            Radgnarack Electric Bike Rack
+          </span>
+        </p>
         <button
           className="px-8 py-2 mt-8 font-bold border border-[#000000] uppercase bg-[#FFC000] text-[#333333] hover:bg-[#8d6a02] hover:text-white shadow-xl rounded-md"
           onClick={() => router.push("/sales")}
         >
           Get yours now
         </button>
-        </div>
-       
-     </div>
+      </div>
     </div>
   );
 };
