7d57c9bbe488b0e0f29a134ddb918d9e651cf3bf
ErikRobles <erikjames69@hotmail.com>
2023-06-21 10:08:50 -0600

    cors fix take 2

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -21,9 +21,6 @@ const Contact = () => {
       url: `${process.env.NEXT_PUBLIC_API_URL}/contact`,
       headers: {
         "Content-Type": "application/json",
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
       },
       data: values,
     };
